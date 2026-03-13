8423ea68efe755341b95e910471ec5ba203ac8c5
JS0801 <jsuthar@oncloudconsulting.ca>
2026-03-13 12:42:57 -0400

    Update from NetSuite (Auto)

--- BC_Sales_Order_Timesheet_V2.js
+++ BC_Sales_Order_Timesheet_V2.js
@@ -1185,7 +1185,8 @@ define([
   }
 
   function formatCurrency(amount) {
-    if (isNaN(amount)) return '$ 0.00';
+    if (isNaN(amount)) return '';
+    if (amount.indexOf('$') != -1) return amount;
     return '$ ' + parseFloat(amount).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   }
 
